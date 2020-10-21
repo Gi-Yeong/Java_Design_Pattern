d629ebeeae08c9411b346dd2d73d4d3a91a919a3
kiyoung85 <kiyoung85@gmail.com>
2020-10-21 23:25:00 +0900

    어댑터 패턴 - 폴더 이름변경

---
+++ Adapter_Pattern/src/com/design/pattern/AdapterImpl.java
@@ -0,0 +1,15 @@
+package com.design.pattern;
+
+public class AdapterImpl implements Adapter {
+
+    @Override
+    public Float twiceOf(Float f) {
+        return Math.doubled(f.doubleValue()).floatValue();
+    }
+
+    @Override
+    public Float halfOf(Float f) {
+        System.out.println("절반 함수 호출 로그");
+        return (float) Math.half(f.doubleValue());
+    }
+}
